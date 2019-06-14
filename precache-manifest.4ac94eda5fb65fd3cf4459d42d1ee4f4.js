299703d715b5cb5ae0b156840bbe9af2e9972e40
Anh Tra <tratrunganh001@gmail.com>
2019-06-15 00:10:28 +0700

    Updates

---
+++ precache-manifest.4ac94eda5fb65fd3cf4459d42d1ee4f4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f2112d2ec35abbdf537c97f73ea1de9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "85c974a34a81a64b6cdb",
+    "url": "/static/css/2.60a6b8e0.chunk.css"
+  },
+  {
+    "revision": "6aedf22b5ea4dc777538",
+    "url": "/static/css/main.d9b92ea5.chunk.css"
+  },
+  {
+    "revision": "85c974a34a81a64b6cdb",
+    "url": "/static/js/2.f4c58b03.chunk.js"
+  },
+  {
+    "revision": "6aedf22b5ea4dc777538",
+    "url": "/static/js/main.f24e4f3e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
